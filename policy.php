20d04bb461c38850a812490f768946494ed355e8
Gary Chen <s3318730@student.rmit.edu.au>
2011-10-05 16:52:27 +1100

    MobileGangster footer pages
    Prototype of footer

---
+++ policy.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html 
+     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<head>
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+	<meta name="description" content="Mobile Online Shop" />
+	<meta name="keywords" content="Latest Mobile, Best Mobile, Mobile Price" />
+	<meta name="author" content="WebGangsters" />
+		
+	<title>MobileGangster | Mobile Online Store</title>
+	
+	<link rel="stylesheet" type="text/css" href="../template.css" />
+		
+</head>
+	
+	
+<body>
+	<div id="container">
+	<!-- Header Start -->
+		<div id="header">
+				<?php include('../header/header.html'); ?>
+		</div>
+	<!-- Header End -->
+
+	<!-- Navigation Start -->
+		<div id="navigation">
+			<ul>
+				<li class="navigation"><a href="../main/index.php">Home</a></li>
+				<li class="navigation"><a href="../register/register.html">Register</a></li>
+				<li class="navigation"><a href="../trips/trip.php">Browse Trips</a></li>
+				<li class="navigation"><a href="../payment/payment.html">Payment</a></li>
+			</ul>
+		</div>
+	<!-- Navigation End -->
+	
+	
+	<!-- Content Start -->
+		<div>
+		<!-- Policy Start -->
+			<div id="policy">
+				<table border="0">
+					<tr>
+						<td><h3>Privacy Policy</h3></td>
+					</tr>
+					<tr>
+						<th>
+							This privacy policy statement has been made in accordance with amendments to 
+							the Federal Government Privacy Amendment (Private Sector) Act 2000 (New Privacy Laws) 
+							which took effect from 21 December 2001 as well as the Spam Act 2003 which took effect 
+							from 10 April 2004.
+						</th>
+					</tr>
+					<tr>
+						<td>
+							Mobile Gangster recognizes the importance of client privacy and supports best practice 
+							procedures in the way your personal information is handled by us. By providing your 
+							personal information you agree and consent to the collection, use, disclosure, storage 
+							and holding of your personal information in accordance with this privacy policy.
+							<br /><br />
+							At Mobile Gangster, we are committed to our clients privacy and security. 
+							We will never pass on your personal details to a third party except the necessary 
+							information required by providers of products or services you have purchased.
+							<br /><br />
+							Although, from time to time, Mobile Gangster may provide statistical information about 
+							sales, trading patterns and information on navigation techniques to reputable third 
+							parties, this does not include any direct personal information identifying you as 
+							our customer.
+							<br /><br />
+							For the Credit Card Information, your credit card details are only used to pay for 
+							a travel booking, and are securely stored. They are not used for any other purpose. 
+							Details are only supplied to a third party when it is required to provide goods or 
+							services which you have purchased.
+						</td>
+					</tr>
+				</table>
+			</div>
+		<!-- Policy End -->
+	
+		</div>
+	<!-- Content End -->
+	
+	<!-- Footer Start -->
+		<div id="footer">
+				<?php include('../footer/footer.html'); ?>
+		</div>
+	<!-- Footer End -->
+	</div>
+</body>
+</html>
---
+++ sitemap.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html 
+     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<head>
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+	<meta name="description" content="Mobile Online Shop" />
+	<meta name="keywords" content="Latest Mobile, Best Mobile, Mobile Price" />
+	<meta name="author" content="WebGangsters" />
+		
+	<title>MobileGangster | Mobile Online Store</title>
+	
+	<link rel="stylesheet" type="text/css" href="../template.css" />
+		
+</head>
+	
+	
+<body>
+	<div id="container">
+	<!-- Header Start -->
+		<div id="header">
+				<?php include('../header/header.html'); ?>
+		</div>
+	<!-- Header End -->
+
+	<!-- Navigation Start -->
+		<div id="navigation">
+			<ul>
+				<li class="navigation"><a href="../main/index.php">Home</a></li>
+				<li class="navigation"><a href="../register/register.html">Register</a></li>
+				<li class="navigation"><a href="../trips/trip.php">Browse Trips</a></li>
+				<li class="navigation"><a href="../payment/payment.html">Payment</a></li>
+			</ul>
+		</div>
+	<!-- Navigation End -->
+	
+	<!-- Site Map Start -->
+		<div id="siteMap">
+			<table border="0">
+				<tr>
+					<td colspan="4" class="siteMapTitle"><img src="../img/sitemap.gif" alt="Site Map" /></td>
+				</tr>
+				<tr>
+					<td class="siteMapMain"><a href="../index.php">Home</a></td>
+					<td class="siteMapMain"><a href="../register/register.html">Register</a></td>
+					<td class="siteMapMain"><a href="../trips/trip.php">Browse Trips</a></td>
+					<td class="siteMapMain"><a href="../payment/payment.html">Payment</a></td>
+				</tr>
+				<tr>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapSub"><a href="../trips/ayersrock.html">Ayers Rock</a></td>
+					<td class="siteMapInvi"> </td>
+				</tr>
+				<tr>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapSub"><a href="../trips/cooberpedy.html">Coober Peddy</a></td>
+					<td class="siteMapInvi"> </td>
+				</tr>
+				<tr>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapSub"><a href="../trips/kalgoorlie.html">Kalgoorlie</a></td>
+					<td class="siteMapInvi"> </td>
+				</tr>
+				<tr>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapSub"><a href="../trips/darwin.html">Darwin</a></td>
+					<td class="siteMapInvi"> </td>
+				</tr>
+				<tr>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapInvi"> </td>
+					<td class="siteMapSub"><a href="../trips/devilsmarble.html">Devils Marbles</a></td>
+					<td class="siteMapInvi"> </td>
+				</tr>
+			</table>
+		</div>	
+	<!-- Site Map End -->
+	
+	<!-- Content End -->
+	
+	<!-- Footer Start -->
+		<div id="footer">
+				<?php include('../footer/footer.html'); ?>
+		</div>
+	<!-- Footer End -->
+	</div>
+</body>
+</html>
